3c964f65f27b4c9afacf0a9d01ace8fc3fae07cb
ebruneton <ebruneton>
2002-10-01 07:55:34 +0000

    fixed typo

--- src/org/objectweb/asm/CodeVisitor.java
+++ src/org/objectweb/asm/CodeVisitor.java
@@ -102,7 +102,7 @@ public interface CodeVisitor {
    * @param opcode the opcode of the type instruction to be visited. This opcode
    *      is either NEW, ANEWARRAY, CHECKCAST or INSTANCEOF.
    * @param desc the operand of the instruction to be visited. This operand is
-   *      must be a fully qualified class name in internal form, or a the type
+   *      must be a fully qualified class name in internal form, or the type
    *      descriptor of an array type (see {@link Type Type}).
    */
 

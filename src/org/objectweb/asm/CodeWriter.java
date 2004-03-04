4991bf748b93b70c08b75b1c0088d244834683c4
ekuleshov <ekuleshov>
2004-03-04 01:22:17 +0000

    Removed duplicated code fragment

--- src/org/objectweb/asm/CodeWriter.java
+++ src/org/objectweb/asm/CodeWriter.java
@@ -513,11 +513,10 @@ public class CodeWriter implements CodeVisitor {
   protected CodeWriter (final ClassWriter cw, final boolean computeMaxs) {
     if (cw.firstMethod == null) {
       cw.firstMethod = this;
-      cw.lastMethod = this;
     } else {
       cw.lastMethod.next = this;
-      cw.lastMethod = this;
     }
+    cw.lastMethod = this;
     this.cw = cw;
     this.computeMaxs = computeMaxs;
     if (computeMaxs) {

909e90acb7f7bc94d79a08bd249df2f779c047dd
ebruneton <ebruneton>
2004-03-19 13:37:52 +0000

    fixed ACC_SYNTHETIC value

--- src/org/objectweb/asm/Constants.java
+++ src/org/objectweb/asm/Constants.java
@@ -46,27 +46,26 @@ public interface Constants {
 
   // access flags
 
-  int ACC_PUBLIC = 1;         // class, field, method
-  int ACC_PRIVATE = 2;        // class, field, method
-  int ACC_PROTECTED = 4;      // class, field, method
-  int ACC_STATIC = 8;         //        field, method
-  int ACC_FINAL = 16;         // class, field, method
-  int ACC_SYNCHRONIZED = 32;  //               method
-  int ACC_VOLATILE = 64;      //        field
-  int ACC_BRIDGE = 64;        //               methods
-  int ACC_VARARGS = 128;      //               methods
-  int ACC_TRANSIENT = 128;    //        field
-  int ACC_NATIVE = 256;       //               methods
-  int ACC_ENUM = 256;         // class(?) field
-  int ACC_ANNOTATION = 2048;  // class
+  int ACC_PUBLIC =       0x0001; // class, field, method
+  int ACC_PRIVATE =      0x0002; // class, field, method
+  int ACC_PROTECTED =    0x0004; // class, field, method
+  int ACC_STATIC =       0x0008; //        field, method
+  int ACC_FINAL =        0x0010; // class, field, method
+  int ACC_SUPER =        0x0020; // class
+  int ACC_SYNCHRONIZED = 0x0020; //               method
+  int ACC_VOLATILE =     0x0040; //        field
+  int ACC_BRIDGE =       0x0040; //               method
+  int ACC_VARARGS =      0x0080; //               method
+  int ACC_TRANSIENT =    0x0080; //        field
+  int ACC_NATIVE =       0x0100; //               method
+  int ACC_INTERFACE =    0x0200; // class
+  int ACC_ABSTRACT =     0x0400; // class,        method
+  int ACC_STRICT =       0x0800; //               method 
+  int ACC_SYNTHETIC =    0x1000; // class, field, method
+  int ACC_ANNOTATION =   0x2000; // class
+  int ACC_ENUM =         0x4000; // class(?) field
   
-  int ACC_INTERFACE = 512;    // class
-  int ACC_ABSTRACT = 1024;    // class,        method
-  int ACC_STRICT = 2048;      //               method 
-  int ACC_SUPER = 32;         // class
-
-  int ACC_SYNTHETIC = 65536;  // class, field, method (0x1000)
-  int ACC_DEPRECATED = 131072;
+  int ACC_DEPRECATED =   131072; // ASM specific pseudo access flag
 
   // types for NEWARRAY
 

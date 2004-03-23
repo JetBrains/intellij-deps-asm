abe065b27a916e9779299e8f953f8b0c1d646f49
ebruneton <ebruneton>
2004-03-23 09:48:07 +0000

    added comment

--- src/org/objectweb/asm/ClassWriter.java
+++ src/org/objectweb/asm/ClassWriter.java
@@ -976,7 +976,9 @@ public class ClassWriter implements ClassVisitor {
 
   /**
    * Adds a name and type to the constant pool of the class being build. Does
-   * nothing if the constant pool already contains a similar item.
+   * nothing if the constant pool already contains a similar item. <i>This
+   * method is intended for {@link Attribute} sub classes, and is normally not
+   * needed by class generators or adapters.</i>
    *
    * @param name a name.
    * @param desc a type descriptor.

6498f319e493512825edd163fc516653eb4d7439
ebruneton <ebruneton>
2004-06-29 07:51:53 +0000

    improved javadoc of visitField method

--- src/org/objectweb/asm/ClassVisitor.java
+++ src/org/objectweb/asm/ClassVisitor.java
@@ -97,9 +97,11 @@ public interface ClassVisitor {
    *      <tt>null</tt> if the field does not have an initial value, must be an
    *      {@link java.lang.Integer Integer}, a {@link java.lang.Float Float}, a
    *      {@link java.lang.Long Long}, a {@link java.lang.Double Double} or a
-   *      {@link String String}. <i>This parameter is only used for static
-   *      fields</i>. Its value is ignored for non static fields, which must be
-   *      initialized through bytecode instructions in constructors or methods.
+   *      {@link String String} (for <tt>int</tt>, <tt>float</tt>, <tt>long</tt>
+   *      or <tt>String</tt> fields respectively). <i>This parameter is only 
+   *      used for static fields</i>. Its value is ignored for non static 
+   *      fields, which must be initialized through bytecode instructions in 
+   *      constructors or methods.
    * @param attrs the non standard method attributes, linked together by their
    *      <tt>next</tt> field. May be <tt>null</tt>.
    */

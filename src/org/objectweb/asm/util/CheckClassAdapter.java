e3417a8e4e3f31fbc252a5f168f50cc466aad2b6
ekuleshov <ekuleshov>
2005-02-03 17:16:50 +0000

    removed unused import

--- src/org/objectweb/asm/util/CheckClassAdapter.java
+++ src/org/objectweb/asm/util/CheckClassAdapter.java
@@ -41,7 +41,6 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Attribute;
-import org.objectweb.asm.Label;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.ClassNode;

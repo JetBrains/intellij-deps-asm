9ee23b5488bbf0813b7cbb586ad25e046d49e2ca
ekuleshov <ekuleshov>
2006-01-04 18:40:57 +0000

    removing inner classes

--- test/conform/org/objectweb/asm/ClassWriterTest3.java
+++ test/conform/org/objectweb/asm/ClassWriterTest3.java
@@ -179,69 +179,21 @@ class ClassInfo {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        cr.accept(new ClassVisitor() {
+        
+        // optimized version
+        int h = cr.header;
+        ClassInfo.this.access = cr.readUnsignedShort(h);
+        char[] buf = new char[2048];
+        // String name = cr.readClass( cr.header + 2, buf);
 
-            public void visit(
-                int version,
-                int access,
-                String name,
-                String signature,
-                String superName,
-                String[] interfaces)
-            {
-                ClassInfo.this.access = access;
-                ClassInfo.this.superClass = superName;
-                ClassInfo.this.interfaces = interfaces;
-            }
-
-            public void visitSource(String source, String debug) {
-            }
-
-            public void visitOuterClass(String owner, String name, String desc)
-            {
-            }
-
-            public AnnotationVisitor visitAnnotation(
-                String desc,
-                boolean visible)
-            {
-                return null;
-            }
-
-            public void visitAttribute(Attribute attr) {
-            }
-
-            public void visitInnerClass(
-                String name,
-                String outerName,
-                String innerName,
-                int access)
-            {
-            }
-
-            public FieldVisitor visitField(
-                int access,
-                String name,
-                String desc,
-                String signature,
-                Object value)
-            {
-                return null;
-            }
-
-            public MethodVisitor visitMethod(
-                int access,
-                String name,
-                String desc,
-                String signature,
-                String[] exceptions)
-            {
-                return null;
-            }
-
-            public void visitEnd() {
-            }
-        }, ClassReader.SKIP_DEBUG);
+        int v = cr.getItem(cr.readUnsignedShort(h + 4));
+        ClassInfo.this.superClass = v == 0 ? null : cr.readUTF8(v, buf);
+        ClassInfo.this.interfaces = new String[cr.readUnsignedShort(h + 6)];
+        h += 8;
+        for (int i = 0; i < interfaces.length; ++i) {
+            interfaces[i] = cr.readClass(h, buf);
+            h += 2;
+        }
     }
 
     String getName() {

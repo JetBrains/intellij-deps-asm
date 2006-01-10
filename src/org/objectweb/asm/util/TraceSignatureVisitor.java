2b93eda6c3f7ed187a255ec57fe44c590733ac63
ebruneton <ebruneton>
2006-01-10 08:06:56 +0000

    patch from Andrei Loskutov
    [[Split portion of a mixed commit.]]

--- src/org/objectweb/asm/util/TraceSignatureVisitor.java
+++ src/org/objectweb/asm/util/TraceSignatureVisitor.java
@@ -136,6 +136,8 @@ public class TraceSignatureVisitor implements SignatureVisitor {
         endFormals();
         if (!seenParameter) {
             declaration.append('(');
+        } else {
+            seenParameter = false;
         }
         declaration.append(')');
         returnType = new StringBuffer();
@@ -202,13 +204,26 @@ public class TraceSignatureVisitor implements SignatureVisitor {
     public void visitClassType(String name) {
         if (!"java/lang/Object".equals(name)) {
             declaration.append(separator).append(name.replace('/', '.'));
+        } else {
+            // Map<java.lang.Object,java.util.List>
+            // or
+            // abstract public V get(Object key); (seen in Dictionary.class)
+            // should have Object
+            // but java.lang.String extends java.lang.Object is unnecessary
+            boolean needObjectClass = argumentStack % 2 == 1 || seenParameter;
+            if (needObjectClass) {
+                declaration.append(separator).append(name.replace('/', '.'));
+            }
         }
         separator = "";
         argumentStack *= 2;
     }
 
     public void visitInnerClassType(String name) {
-        // TODO
+        // TODO tests
+        declaration.append(separator).append(name.replace('/', '.'));
+        separator = "";
+        argumentStack *= 2;
     }
 
     public void visitTypeArgument() {

56561bec96140774029bb102b8ec7fc29fa60bd2
andrei <andrei>
2006-04-12 22:09:34 +0000

    introduced method to allow subclasses use its own method visitor

--- src/org/objectweb/asm/util/ASMifierClassVisitor.java
+++ src/org/objectweb/asm/util/ASMifierClassVisitor.java
@@ -48,28 +48,28 @@ import org.objectweb.asm.Opcodes;
  * {@link #main main} method);</li> <li>edit the generated source code, if
  * necessary.</li> </ul> The source code printed when visiting the
  * <tt>Hello</tt> class is the following: <p> <blockquote>
- * 
+ *
  * <pre>
  * import org.objectweb.asm.*;
- * 
+ *
  * public class HelloDump implements Opcodes {
- * 
+ *
  *     public static byte[] dump() throws Exception {
- * 
+ *
  *         ClassWriter cw = new ClassWriter(0);
  *         FieldVisitor fv;
  *         MethodVisitor mv;
  *         AnnotationVisitor av0;
- * 
+ *
  *         cw.visit(49,
  *                 ACC_PUBLIC + ACC_SUPER,
  *                 &quot;Hello&quot;,
  *                 null,
  *                 &quot;java/lang/Object&quot;,
  *                 null);
- * 
+ *
  *         cw.visitSource(&quot;Hello.java&quot;, null);
- * 
+ *
  *         {
  *             mv = cw.visitMethod(ACC_PUBLIC, &quot;&lt;init&gt;&quot;, &quot;()V&quot;, null, null);
  *             mv.visitVarInsn(ALOAD, 0);
@@ -101,26 +101,26 @@ import org.objectweb.asm.Opcodes;
  *             mv.visitEnd();
  *         }
  *         cw.visitEnd();
- * 
+ *
  *         return cw.toByteArray();
  *     }
  * }
- * 
+ *
  * </pre>
- * 
+ *
  * </blockquote> where <tt>Hello</tt> is defined by: <p> <blockquote>
- * 
+ *
  * <pre>
  * public class Hello {
- * 
+ *
  *     public static void main(String[] args) {
  *         System.out.println(&quot;hello&quot;);
  *     }
  * }
  * </pre>
- * 
+ *
  * </blockquote>
- * 
+ *
  * @author Eric Bruneton
  * @author Eugene Kuleshov
  */
@@ -152,9 +152,9 @@ public class ASMifierClassVisitor extends ASMifierAbstractVisitor implements
      * Prints the ASM source code to generate the given class to the standard
      * output. <p> Usage: ASMifierClassVisitor [-debug] &lt;fully qualified
      * class name or class file name&gt;
-     * 
+     *
      * @param args the command line arguments.
-     * 
+     *
      * @throws Exception if the class cannot be found, or if an IO exception
      *         occurs.
      */
@@ -193,7 +193,7 @@ public class ASMifierClassVisitor extends ASMifierAbstractVisitor implements
 
     /**
      * Constructs a new {@link ASMifierClassVisitor} object.
-     * 
+     *
      * @param pw the print writer to be used to print the class.
      */
     public ASMifierClassVisitor(final PrintWriter pw) {
@@ -382,10 +382,14 @@ public class ASMifierClassVisitor extends ASMifierAbstractVisitor implements
         }
         buf.append(");\n");
         text.add(buf.toString());
-        ASMifierMethodVisitor acv = new ASMifierMethodVisitor();
+        ASMifierMethodVisitor acv = createASMifierMethodVisitor();
         text.add(acv.getText());
         text.add("}\n");
         return acv;
+    }
+
+    protected ASMifierMethodVisitor createASMifierMethodVisitor() {
+        return new ASMifierMethodVisitor();
     }
 
     public AnnotationVisitor visitAnnotation(
@@ -422,7 +426,7 @@ public class ASMifierClassVisitor extends ASMifierAbstractVisitor implements
     /**
      * Appends a string representation of the given access modifiers to {@link
      * #buf buf}.
-     * 
+     *
      * @param access some access modifiers.
      */
     void appendAccess(final int access) {

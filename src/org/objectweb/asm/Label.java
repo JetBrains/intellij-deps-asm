9a85184563afac06670e16ed169290dff6d0c388
ebruneton <ebruneton>
2006-07-21 13:20:57 +0000

    fixed Javadoc of inputStackTop field

--- src/org/objectweb/asm/Label.java
+++ src/org/objectweb/asm/Label.java
@@ -155,9 +155,7 @@ public class Label {
      * semantics of this field depends on the algorithm that is used.
      * 
      * When only the maximum stack size is computed, this field is the number of
-     * elements in the input stack, plus one. Basic blocks that have never been
-     * visited have a null inputStackTop, and can therefore be distinguished
-     * from labels whose inputStackTop has already been computed.
+     * elements in the input stack.
      * 
      * When the stack map frames are completely computed, this field is the
      * offset of the first output stack element relatively to the top of the

5824f4a08157591e7a41154a5f772a701277c1e5
ebruneton <ebruneton>
2006-07-28 07:15:15 +0000

    fixed bug in visitLocalVariable (update max locals: 'L' was used instead of 'J' for 'long')
    [[Split portion of a mixed commit.]]

--- src/org/objectweb/asm/MethodWriter.java
+++ src/org/objectweb/asm/MethodWriter.java
@@ -1161,7 +1161,7 @@ class MethodWriter implements MethodVisitor {
         if (compute != NOTHING) {
             // updates max locals
             char c = desc.charAt(0);
-            int n = index + (c == 'L' || c == 'D' ? 2 : 1);
+            int n = index + (c == 'J' || c == 'D' ? 2 : 1);
             if (n > maxLocals) {
                 maxLocals = n;
             }

b3f5d5eef0097c48a19f67b1f9dcda1c86eff8ab
ekuleshov <ekuleshov>
2009-06-11 00:35:07 +0000

    optimized ClassReader.readUTF() implementation

--- src/org/objectweb/asm/ClassReader.java
+++ src/org/objectweb/asm/ClassReader.java
@@ -1936,39 +1936,39 @@ public class ClassReader {
         int endIndex = index + utfLen;
         byte[] b = this.b;
         int strLen = 0;
-        int c, d, e;
+        int c;
+        int st = 0;
+        char cc = 0;
         while (index < endIndex) {
-            c = b[index++] & 0xFF;
-            switch (c >> 4) {
+            c = b[index++];
+            switch (st) {
                 case 0:
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                    // 0xxxxxxx
-                    buf[strLen++] = (char) c;
+                    c = c & 0xFF;
+                    if (c < 0x80) {  // 0xxxxxxx
+                        buf[strLen++] = (char) c;
+                    } else if (c < 0xE0 && c > 0xBF) {  // 110x xxxx 10xx xxxx
+                        cc = (char) (c & 0x1F);
+                        st = 1;
+                    } else {  // 1110 xxxx 10xx xxxx 10xx xxxx
+                        cc = (char) (c & 0x0F);
+                        st = 2;
+                    }
                     break;
-                case 12:
-                case 13:
-                    // 110x xxxx 10xx xxxx
-                    d = b[index++];
-                    buf[strLen++] = (char) (((c & 0x1F) << 6) | (d & 0x3F));
+                    
+                case 1:  // byte 2 of 2-byte char or byte 3 of 3-byte char 
+                    buf[strLen++] = (char) ((cc << 6) | (c & 0x3F));
+                    st = 0;
                     break;
-                default:
-                    // 1110 xxxx 10xx xxxx 10xx xxxx
-                    d = b[index++];
-                    e = b[index++];
-                    buf[strLen++] = (char) (((c & 0x0F) << 12)
-                            | ((d & 0x3F) << 6) | (e & 0x3F));
+                    
+                case 2:  // byte 2 of 3-byte char
+                    cc = (char) ((cc << 6) | (c & 0x3F));
+                    st = 1;
                     break;
             }
         }
         return new String(buf, 0, strLen);
     }
-
+    
     /**
      * Reads a class constant pool item in {@link #b b}. <i>This method is
      * intended for {@link Attribute} sub classes, and is normally not needed by

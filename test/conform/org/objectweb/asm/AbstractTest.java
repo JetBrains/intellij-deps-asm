d307fdced3badd0e06645529119206d832b570c3
ebruneton <ebruneton>
2011-10-25 17:36:07 +0000

    added missing Google copyright in previous commits

--- test/conform/org/objectweb/asm/AbstractTest.java
+++ test/conform/org/objectweb/asm/AbstractTest.java
@@ -1,6 +1,7 @@
 /***
  * ASM tests
  * Copyright (c) 2000-2011 INRIA, France Telecom
+ * Copyright (c) 2011 Google
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

937167f3736ebd0cb62185a1fc16855c10a7b736
Eric Bruneton <ebruneton@free.fr>
2017-09-21 09:50:14 +0200

    AALOAD should give null when the input array is null.

--- src/org/objectweb/asm/Frame.java
+++ src/org/objectweb/asm/Frame.java
@@ -1056,7 +1056,7 @@ class Frame {
         case Opcodes.AALOAD:
             pop(1);
             t1 = pop();
-            push(ELEMENT_OF + t1);
+            push(t1 == NULL ? t1 : ELEMENT_OF + t1);
             break;
         case Opcodes.ISTORE:
         case Opcodes.FSTORE:

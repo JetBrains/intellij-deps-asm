fd883951e9e95d7287da8828075b0f6cd4800a87
Eric Bruneton <ebruneton@free.fr>
2017-12-28 09:17:38 +0100

    Improve the class generation benchmark by using the JMH framework.
    Results:
    Benchmark                             Mode  Cnt       Score      Error  Units
    GeneratorBenchmark.asm4_0            thrpt   20  753119,277 ± 1890,328  ops/s
    GeneratorBenchmark.asm5_0            thrpt   20  729869,073 ± 2328,108  ops/s
    GeneratorBenchmark.asm6_0            thrpt   20  728330,694 ± 2244,506  ops/s
    GeneratorBenchmark.asm6_1            thrpt   20  847886,985 ± 2767,943  ops/s
    GeneratorBenchmark.aspectJBcel       thrpt   20  274363,976 ± 1171,862  ops/s
    GeneratorBenchmark.bcel              thrpt   20  196715,573 ±  797,970  ops/s
    GeneratorBenchmark.cojen             thrpt   20  191329,604 ±  898,662  ops/s
    GeneratorBenchmark.csgBytecode       thrpt   20  325157,840 ± 1065,897  ops/s
    GeneratorBenchmark.gnuByteCode       thrpt   20  245634,219 ± 1061,192  ops/s
    GeneratorBenchmark.jclassLib         thrpt   20  280383,253 ±  973,727  ops/s
    GeneratorBenchmark.jiapi             thrpt   20   75012,298 ±  325,473  ops/s
    GeneratorBenchmark.mozillaClassFile  thrpt   20  303011,386 ± 1629,259  ops/s

---
+++ benchmarks/write/src/jmh/java/org/objectweb/asm/benchmarks/JiapiGenerator.java
@@ -0,0 +1,85 @@
+// ASM: a very small and fast Java bytecode manipulation framework
+// Copyright (c) 2000-2011 INRIA, France Telecom
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holders nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+package org.objectweb.asm.benchmarks;
+
+import java.lang.reflect.Modifier;
+
+import net.sf.jiapi.reflect.InstructionFactory;
+import net.sf.jiapi.reflect.InstructionList;
+import net.sf.jiapi.reflect.JiapiClass;
+import net.sf.jiapi.reflect.JiapiMethod;
+import net.sf.jiapi.reflect.Signature;
+
+/**
+ * A "Hello World!" class generator using the Jiapi library.
+ *
+ * @author Eric Bruneton
+ */
+public class JiapiGenerator implements Generator {
+
+  private static final Signature EMPTY_SIGNATURE = new Signature("()V");
+
+  private static final Signature MAIN_SIGNATURE = new Signature("([Ljava/lang/String;)V");
+
+  private static final Signature PRINTLN_SIGNATURE = new Signature("(Ljava/lang/String;)V");
+
+  @Override
+  public String getVersion() {
+    return null;
+  }
+
+  @Override
+  public byte[] generateClass() {
+    try {
+      JiapiClass jiapiClass = JiapiClass.createClass("HelloWorld");
+
+      // No API to set SourceFile!
+
+      JiapiMethod method = jiapiClass.addMethod(Modifier.PUBLIC, "<init>", EMPTY_SIGNATURE);
+      InstructionList insnList = method.getInstructionList();
+      InstructionFactory insnFactory = insnList.getInstructionFactory();
+      insnList.add(insnFactory.aload(0));
+      insnList.add(insnFactory.invoke(0, "java/lang/Object", "<init>", EMPTY_SIGNATURE));
+      insnList.add(insnFactory.returnMethod(method));
+
+      method = jiapiClass.addMethod(Modifier.PUBLIC | Modifier.STATIC, "main", MAIN_SIGNATURE);
+      insnList = method.getInstructionList();
+      insnFactory = insnList.getInstructionFactory();
+      insnList.add(
+          insnFactory.getField(
+              Modifier.STATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+      insnList.add(insnFactory.pushConstant("Hello world!"));
+      insnList.add(insnFactory.invoke(0, "java/io/PrintStream", "println", PRINTLN_SIGNATURE));
+      insnList.add(insnFactory.returnMethod(method));
+
+      return jiapiClass.getByteCode();
+    } catch (Exception e) {
+      throw new RuntimeException("Class generation failed", e);
+    }
+  }
+}

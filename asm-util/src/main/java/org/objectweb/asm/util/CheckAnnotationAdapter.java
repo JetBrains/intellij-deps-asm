e03c024bafd825e86d01f0df4cc7c989cdd86630
Eric Bruneton <ebruneton@free.fr>
2018-01-21 14:45:09 +0100

    Restore bug fix, make previously added test pass.

--- asm-util/src/main/java/org/objectweb/asm/util/CheckAnnotationAdapter.java
+++ asm-util/src/main/java/org/objectweb/asm/util/CheckAnnotationAdapter.java
@@ -80,9 +80,7 @@ public class CheckAnnotationAdapter extends AnnotationVisitor {
         || value instanceof double[])) {
       throw new IllegalArgumentException("Invalid annotation value");
     }
-    if (value instanceof Type
-        && ((Type) value).getSort() != Type.OBJECT
-        && ((Type) value).getSort() != Type.ARRAY) {
+    if (value instanceof Type && ((Type) value).getSort() == Type.METHOD) {
       throw new IllegalArgumentException("Invalid annotation value");
     }
     super.visit(name, value);

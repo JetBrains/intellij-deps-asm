bca84006f9cbc52e9c429b4500e94a8e2c91dbe5
Eric Bruneton <ebruneton@free.fr>
2018-09-10 19:55:03 +0200

    Use the memory profiler only for the memory benchmarks. This reduces the time…

--- benchmarks/src/jmh/java/org/objectweb/asm/benchmarks/AdapterBenchmark.java
+++ benchmarks/src/jmh/java/org/objectweb/asm/benchmarks/AdapterBenchmark.java
@@ -45,7 +45,7 @@ import org.openjdk.jmh.infra.Blackhole;
  */
 @Fork(1)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 30, time = 1, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Thread)
 public class AdapterBenchmark extends AbstractBenchmark {
 
--- benchmarks/src/jmh/java/org/objectweb/asm/benchmarks/GeneratorBenchmark.java
+++ benchmarks/src/jmh/java/org/objectweb/asm/benchmarks/GeneratorBenchmark.java
@@ -44,7 +44,7 @@ import org.openjdk.jmh.annotations.Warmup;
  */
 @Fork(1)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 30, time = 1, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Thread)
 public class GeneratorBenchmark extends AbstractBenchmark {
 

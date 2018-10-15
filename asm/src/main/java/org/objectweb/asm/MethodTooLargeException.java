7989589b696779f510827ad4f1569464974096f5
forax <forax@univ-mlv.fr>
2018-10-15 23:03:42 +0200

    fix warning because the serialUID is not set at compiler time

--- asm/src/main/java/org/objectweb/asm/MethodTooLargeException.java
+++ asm/src/main/java/org/objectweb/asm/MethodTooLargeException.java
@@ -34,6 +34,7 @@ package org.objectweb.asm;
  * @author Jason Zaugg
  */
 public final class MethodTooLargeException extends IndexOutOfBoundsException {
+  private static final long serialVersionUID = 6807380416709738314L;
 
   private final String className;
   private final String methodName;

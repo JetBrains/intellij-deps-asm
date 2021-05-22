0c3dbd42c1af447a14cec11bae6a416828e54056
Eric Bruneton <ebruneton@free.fr>
2021-05-22 13:05:00 +0200

    Fix the Javadoc of LdcInsnNode.

--- asm-tree/src/main/java/org/objectweb/asm/tree/LdcInsnNode.java
+++ asm-tree/src/main/java/org/objectweb/asm/tree/LdcInsnNode.java
@@ -28,8 +28,11 @@
 package org.objectweb.asm.tree;
 
 import java.util.Map;
+import org.objectweb.asm.ConstantDynamic;
+import org.objectweb.asm.Handle;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
 /**
  * A node that represents an LDC instruction.
@@ -39,17 +42,23 @@ import org.objectweb.asm.Opcodes;
 public class LdcInsnNode extends AbstractInsnNode {
 
   /**
-   * The constant to be loaded on the stack. This parameter must be a non null {@link Integer}, a
-   * {@link Float}, a {@link Long}, a {@link Double}, a {@link String} or a {@link
-   * org.objectweb.asm.Type}.
+   * The constant to be loaded on the stack. This field must be a non null {@link Integer}, a {@link
+   * Float}, a {@link Long}, a {@link Double}, a {@link String}, a {@link Type} of OBJECT or ARRAY
+   * sort for {@code .class} constants, for classes whose version is 49, a {@link Type} of METHOD
+   * sort for MethodType, a {@link Handle} for MethodHandle constants, for classes whose version is
+   * 51 or a {@link ConstantDynamic} for a constant dynamic for classes whose version is 55.
    */
   public Object cst;
 
   /**
    * Constructs a new {@link LdcInsnNode}.
    *
-   * @param value the constant to be loaded on the stack. This parameter must be a non null {@link
-   *     Integer}, a {@link Float}, a {@link Long}, a {@link Double} or a {@link String}.
+   * @param value the constant to be loaded on the stack. This parameter mist be a non null {@link
+   *     Integer}, a {@link Float}, a {@link Long}, a {@link Double}, a {@link String}, a {@link
+   *     Type} of OBJECT or ARRAY sort for {@code .class} constants, for classes whose version is
+   *     49, a {@link Type} of METHOD sort for MethodType, a {@link Handle} for MethodHandle
+   *     constants, for classes whose version is 51 or a {@link ConstantDynamic} for a constant
+   *     dynamic for classes whose version is 55.
    */
   public LdcInsnNode(final Object value) {
     super(Opcodes.LDC);
